ecdebf9dcd215337c01c9bbb6a5f6901841c4cef
Adrian Conlin <aconlin@scottlogic.com>
2016-03-17 11:46:17 +0000

    Restructure e2e test dirs to prevent page objects being treated as tests.

--- src/e2e/tests/App.js
+++ src/e2e/tests/App.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	'Check for root-element': browser => {
-		let home = browser.page.home().navigate();
+		let home = browser.page.Home().navigate();
 
 		home.expect.element('@rootElement').to.be.present.before(1000);
 
@@ -10,7 +10,7 @@ module.exports = {
 	},
 
 	'Check for content': browser => {
-		let home = browser.page.home().navigate();
+		let home = browser.page.Home().navigate();
 
 		home.expect.element('@contentElement').to.be.present.before(1000);
 		home.expect.element('@contentElement').text.to.contain('some page content');
