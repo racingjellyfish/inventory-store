a47dcf3a17ef2c358dce83cca626157d3b770d28
Adrian Conlin <aconlin@scottlogic.com>
2016-03-17 15:12:58 +0000

    Minor updates: - minify the JS bundle for PROD - add source maps - configure the npm build command.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 'use strict';
 
 let path = require('path');
+let webpack = require("webpack");
 let webpackConfigCommon = require('./webpack.config.common');
 
+var IN_PROD = (process.env.NODE_ENV === 'production');
+
 module.exports = {
+	devtool: 'source-map',
 	entry: './src/app/App.js',
+	module: webpackConfigCommon.module,
 	output: {
 		path: path.join(__dirname, 'public/js'),
-		filename: 'bundle.js'
+		filename: IN_PROD ? 'bundle.min.js' : 'bundle.js'
 	},
-	module: webpackConfigCommon.module
+	plugins: IN_PROD ? [
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+	] : []
 };
