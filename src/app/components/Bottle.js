8575e27b97d44e068dbd8ad03fb2287afd704ec6
Adrian Conlin <aconlin@scottlogic.com>
2016-04-04 14:41:31 +0100

    Generate conditionally rendered content outside of the return statement.

--- src/app/components/Bottle.js
+++ src/app/components/Bottle.js
@@ -12,21 +12,24 @@ import shallowCompare from 'react-addons-shallow-compare';
 export default class Bottle extends React.Component {
 	render() {
 		const { id, contents, status, onDelete, onDrink } = this.props;
+
+		// TODO: some sort of generic button component?
+		const drinkButton = contents === 'empty' ?
+			<span className='button disabled'>Drink</span> :
+			<span className='button enabled' onClick={() => onDrink(id)}>Drink</span>;
+
+		// TODO: replace with spinner overlay?
+		const statusUi = status !== 'OK' ?
+			<span className='status'>Status: {status}</span> : undefined;
+
 		return (
 			<li className='bottle'>
 				<span className='contents'>
 					contents: {contents}
 				</span>
-				{
-					// TODO: check this is a sensible approach
-					contents === 'empty' ?
-						<span className='button disabled'>Drink</span>
-					:
-						<span className='button enabled'
-							onClick={() => onDrink(id)}>Drink</span>
-				}
+				{drinkButton}
 				<span className='button' onClick={() => onDelete(id)}>Delete</span>
-				<span className='status'>Status: {status}</span>
+				{statusUi}
 			</li>
 		);
 	};
